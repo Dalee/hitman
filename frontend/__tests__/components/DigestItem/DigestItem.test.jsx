b75d6f03a9b62d71ab89f98bb8e2a9bc38b443cb
Nikita Safonov <ns3777k@gmail.com>
2017-02-16 12:49:26 +0300

    DigestItem tests

--- frontend/__tests__/components/DigestItem/DigestItem.test.jsx
+++ frontend/__tests__/components/DigestItem/DigestItem.test.jsx
@@ -1,17 +1,59 @@
 import React from 'react';
 import DigestItem from '../../../components/DigestItem/DigestItem';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 describe('DigestItem', () => {
 
+    it('should render removal confirmation', () => {
+        const wrapper = shallow(<DigestItem path="some/path" name="test" tags={['latest']} deleteTag={jest.fn()} />);
+        wrapper.find('Button').first().simulate('click');
+
+        expect(wrapper.state('deleteConfirm')).toBe(true);
+    });
+
     it('should remove tag', () => {
         const onButtonClick = jest.fn(() => Promise.resolve());
-        const wrapper = mount(<DigestItem path="some/path" name="test" tags={['latest']} deleteTag={onButtonClick} />);
+        const wrapper = shallow(<DigestItem path="some/path" name="test" tags={['latest']} deleteTag={onButtonClick} />);
         wrapper.setState({deleteConfirm: true});
+        wrapper.find('Button').last().simulate('click');
 
-        wrapper.find('button').last().simulate('click');
         expect(onButtonClick).toHaveBeenCalledTimes(1);
         expect(onButtonClick).toHaveBeenCalledWith('some/path', 'test');
+    });
+
+    it('should restore state after removal', () => {
+        const promise = Promise.resolve();
+        const onButtonClick = jest.fn(() => promise);
+        const wrapper = shallow(<DigestItem path="some/path" name="test" tags={['latest']} deleteTag={onButtonClick} />);
+        wrapper.setState({deleteConfirm: true});
+        wrapper.find('Button').last().simulate('click');
+
+        return promise
+            .then(() => {
+                wrapper.update();
+                expect(wrapper.state('deleteInProgress')).toBe(false);
+                expect(wrapper.state('deleteConfirm')).toBe(false);
+            });
+    });
+
+    it('should hide removal confirmation', () => {
+        const wrapper = shallow(<DigestItem path="some/path" name="test" tags={['latest']} deleteTag={jest.fn()} />);
+        wrapper.setState({deleteConfirm: true});
+        wrapper.find('Button').first().simulate('click');
+
+        expect(wrapper.state('deleteConfirm')).toBe(false);
+    });
+
+    it('should render tags', () => {
+        const tags = ['latest', 'v.1.2.0', 'master-wip'];
+        const wrapper = shallow(<DigestItem path="some/path"
+                                          name="test"
+                                          tags={tags}
+                                          deleteTag={jest.fn()} />);
+
+        wrapper.find('Label').forEach((node, idx) => {
+            expect(node.html()).toMatch(tags[idx]);
+        });
     });
 
 });
--- frontend/__tests__/components/DigestList/DigestList.test.jsx
+++ frontend/__tests__/components/DigestList/DigestList.test.jsx
@@ -5,22 +5,22 @@ import {Loader} from 'semantic-ui-react';
 
 describe('DigestList', () => {
 
-    it('renders error is flag is passed', () => {
+    it('should render error is flag is passed', () => {
         const component = shallow(<DigestList isError />);
         expect(component.equals(<h2>Error :(</h2>)).toBe(true);
     });
 
-    it('renders loading spinner is flag is passed', () => {
+    it('should render loading spinner is flag is passed', () => {
         const component = shallow(<DigestList isLoading />);
         expect(component.equals(<Loader active inline="centered">Loading</Loader>)).toBe(true);
     });
 
-    it('renders image selection request if it\'s not selected', () => {
+    it('should render image selection request if it\'s not selected', () => {
         const component = shallow(<DigestList />);
         expect(component.equals(<h2>Please select image</h2>)).toBe(true);
     });
 
-    it('renders empty repository message with path', () => {
+    it('should render empty repository message with path', () => {
         const props = {
             digest: {children: []},
             path: 'dalee/repo'
@@ -31,7 +31,7 @@ describe('DigestList', () => {
         expect(component.text()).toMatch(registryPath);
     });
 
-    it('renders children as DigestItem', () => {
+    it('should render children as DigestItem', () => {
         const props = {
             digest: {
                 children: [
